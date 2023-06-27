2c4eeb037d792f672303e344361fbd96a7265128
gabrielepetralia <gabri.petralia03@gmail.com>
2023-06-27 15:50:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import "@fortawesome/fontawesome-free/css/all.css"
+import "@fortawesome/fontawesome-free/js/all.js"
+
+export default {
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Boolfolio</h1>
+  </div>
+</template>
+
+<style lang="scss">
+  @use "./scss/main.scss";
+</style>
